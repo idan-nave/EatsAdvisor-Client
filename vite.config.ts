91ffd833d6e5ada406e9d735719882c553194e95
idan <idan1980394@gmail.com>
2025-03-04 11:02:58 +0200

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tsconfigPaths()],
+  resolve: {
+    alias: {
+      '@components': '/src/components',
+      '@hooks': '/src/hooks',
+      '@pages': '/src/pages',
+      '@services': '/src/services',
+      '@types': '/src/types',
+      '@utils': '/src/utils'
+    }
+  }
 })
