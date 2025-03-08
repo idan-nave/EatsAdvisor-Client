5fc958bda880b454a5658a96b7b706e4b6a7ef38
= <=>
2025-03-08 13:00:06 +0200

    wip

--- src/components/PrivateRoute/PrivateRoute.tsx
+++ src/components/PrivateRoute/PrivateRoute.tsx
@@ -1,14 +1,27 @@
 import { Navigate, useLocation } from 'react-router-dom'
 import { useAuth } from '@context'
 import { JSX } from 'react'
+import { useEffect, useState } from 'react'
 
 interface PrivateRouteProps {
   children: JSX.Element
 }
 
 const PrivateRoute: React.FC<PrivateRouteProps> = ({ children }) => {
-  const { user } = useAuth()
+  const { user, loading } = useAuth()
   const location = useLocation()
+
+  const [isChecking, setIsChecking] = useState(true)
+
+  useEffect(() => {
+    if (!loading) {
+      setIsChecking(false)
+    }
+  }, [loading])
+
+  if (isChecking) {
+    return <div>Loading...</div>
+  }
 
   if (location.pathname === '/scan' && location.state?.allowed !== true) {
     return <Navigate to="/" replace />
