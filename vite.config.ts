58f947d74cc81099f5cf370e332138273e6edaa6
idan <idan1980394@gmail.com>
2025-03-10 03:31:33 +0200

    @Aliases fix final

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,27 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths'
 import basicSSL from '@vitejs/plugin-basic-ssl'
+import path from 'path';
 
 export default defineConfig({
   plugins: [react(), tsconfigPaths(), basicSSL()],
+  resolve: {
+    alias: {
+      '@styles': path.resolve(__dirname, './src/styles'),
+      '@components': path.resolve(__dirname, './src/components'),
+      '@router': path.resolve(__dirname, './src/router'),
+      '@constants': path.resolve(__dirname, './src/constants'),
+      '@hooks': path.resolve(__dirname, './src/hooks'),
+      '@pages': path.resolve(__dirname, './src/pages'),
+      '@api': path.resolve(__dirname, './src/api'),
+      '@context': path.resolve(__dirname, './src/context'),
+      '@layouts': path.resolve(__dirname, './src/layouts'),
+      '@types': path.resolve(__dirname, './src/types'),
+      '@interfaces': path.resolve(__dirname, './src/interfaces'),
+    },
+  },
   server: {
     port: 3001, // Change to port 3001
-    https: true
+    https: {}
   },
 })
