1fb8d7f6764ac6fa0667019dab62253680408812
idan <idan1980394@gmail.com>
2025-03-10 05:51:26 +0200

    default to PORT 3001

--- vite.config.ts
+++ vite.config.ts
@@ -1,34 +1,38 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
-export default defineConfig({
-  server: {
-    https: {},
-  },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src'),
-      '@context': path.resolve(__dirname, 'src/context'),
-      '@interfaces': path.resolve(__dirname, 'src/interfaces'),
-      '@constants': path.resolve(__dirname, 'src/constants'),
-      '@pages': path.resolve(__dirname, 'src/pages'),
-      '@components': path.resolve(__dirname, 'src/components'),
-      '@router': path.resolve(__dirname, 'src/router'),
-      '@layouts': path.resolve(__dirname, 'src/layouts'),
-      '@types': path.resolve(__dirname, 'src/types'),
-      '@api': path.resolve(__dirname, 'src/api'),
-      '@styles': path.resolve(__dirname, 'src/styles'),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    server: {
+      port: parseInt(env.PORT || '3001'),
+      https: {},
     },
-  },
-  plugins: [react(), tsconfigPaths()],
-  build: {
-    rollupOptions: {
-      external: [
-        '@pages/index',
-        'api/api',
-      ],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, 'src'),
+        '@context': path.resolve(__dirname, 'src/context'),
+        '@interfaces': path.resolve(__dirname, 'src/interfaces'),
+        '@constants': path.resolve(__dirname, 'src/constants'),
+        '@pages': path.resolve(__dirname, 'src/pages'),
+        '@components': path.resolve(__dirname, 'src/components'),
+        '@router': path.resolve(__dirname, 'src/router'),
+        '@layouts': path.resolve(__dirname, 'src/layouts'),
+        '@types': path.resolve(__dirname, 'src/types'),
+        '@api': path.resolve(__dirname, 'src/api'),
+        '@styles': path.resolve(__dirname, 'src/styles'),
+      },
     },
-  },
+    plugins: [react(), tsconfigPaths()],
+    build: {
+      rollupOptions: {
+        external: [
+          '@pages/index',
+          'api/api',
+        ],
+      },
+    },
+  };
 });
